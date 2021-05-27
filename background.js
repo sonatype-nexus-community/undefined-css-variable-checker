e2068f44e1937b7ceb7c049137c988df124e0d42
Ross Pokorny <rpokorny@sonatype.com>
2021-05-27 14:54:28 -0400

    Attempt to add logging

--- background.js
+++ background.js
@@ -1,8 +1,12 @@
 // This service worker has permissions to execute code on the page in question. This listener does so
 // any time the devtools page says to.
+console.log('in background.js');
 chrome.runtime.onConnect.addListener(function(devToolsConnection) {
+console.log('in connect listener');
   function devToolsListener(message) {
+    console.log('in devtools listener');
     if (message.type === 'check-css-vars') {
+      console.log('executing script');
       chrome.tabs.executeScript(message.tabId, { file: 'checker.js' });
     }
   }
--- panel.js
+++ panel.js
@@ -1,7 +1,9 @@
 // This script that runs in the devtools itself is not allowed to interact directly with the
 // page, it must go through the registered service worker. At the same time, only this script knows the tabId
 // of the page
+console.log('in panel.js');
 document.getElementById('check-btn').addEventListener('click', function() {
+  console.log('in button handler');
   chrome.runtime.sendMessage({
     type: 'check-css-vars',
     tabId: chrome.devtools.inspectedWindow.tabId
