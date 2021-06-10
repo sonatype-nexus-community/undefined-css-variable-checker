0b5b117e8769f84ebe808457d88e9495426a6557
Ross Pokorny <rpokorny@sonatype.com>
2021-06-10 10:01:03 -0400

    Get fundamental functionality working

--- panel.js
+++ panel.js
@@ -1,10 +1,12 @@
 // This script that runs in the devtools itself is not allowed to interact directly with the
 // page, it must go through the registered service worker. At the same time, only this script knows the tabId
 // of the page
-console.log('in panel.js');
+const port = chrome.runtime.connect({
+  name: 'css-custom-property-checker'
+});
+
 document.getElementById('check-btn').addEventListener('click', function() {
-  console.log('in button handler');
-  chrome.runtime.sendMessage({
+  port.postMessage({
     type: 'check-css-vars',
     tabId: chrome.devtools.inspectedWindow.tabId
   });
